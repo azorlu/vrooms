f5f54717f7e83e29b414db65514f4f95466d60d5
azorlu <mandelbug@gmail.com>
2015-01-30 17:28:37 +0900

    Implemented Bootstrap accordion control in BookSummary partial view

--- Vrooms.Database/dbo/Tables/Languages.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Languages] (
-    [LanguageId]      INT           IDENTITY (1, 1) NOT NULL,
-    [Iso3]            CHAR (3)      NOT NULL,
-    [LanguageName_En] NVARCHAR (64) NOT NULL,
-    PRIMARY KEY CLUSTERED ([LanguageId] ASC)
-);
-
--- Vrooms.Database/dbo/Tables/Patrons.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Patrons] (
-    [PatronId]      INT           IDENTITY (1, 1) NOT NULL,
-    [PersonId]      INT           NOT NULL,
-    [PatronBarcode] NVARCHAR (30) NOT NULL,
-    PRIMARY KEY CLUSTERED ([PatronId] ASC),
-    FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Persons] ([PersonId])
-);
-
